9c1c9811b1c5884f053a3dbd7ff3bfd57dd6af65
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-03-02 20:31:07 +0800

    Sway configs added; some bugfixes after real reinstallation.

--- .init.sh
+++ .init.sh
@@ -1,11 +1,15 @@
 echo WARNING: ONLY BARE MACHINES CAN EXECUTE THIS SCRIPT FOR AUTOMATIC CONFIGURATION
 echo By typing your password, you admit you are fully aware of the risk, and we start.
-chsh -s `which zsh`
 
 sudo sed -i 's/#zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
 sudo locale-gen
 
-sudo pacman -Sy git
+sudo pacman -Sy git clash
+
+curl https://update.glados-config.com/clash/290141/765f3fd/50821/glados-terminal.yaml > .proxy.yaml
+sed -i 's/external-ui: dashboard/# external-ui: dashboard/g' .proxy.yaml
+clash -f .proxy.yaml -d . &
+
 cd ~
 git clone -n https://github.com/ProfXv/home_configs
 mv -i home_configs/.git/ .
@@ -13,10 +17,6 @@ rm -r home_configs/
 git checkout -f
 git remote remove origin
 git remote add origin git@github.com:ProfXv/home_configs.git
-
-curl https://update.glados-config.com/clash/290141/765f3fd/50821/glados-terminal.yaml > .proxy.yaml
-sed -i 's/external-ui: dashboard/# external-ui: dashboard/g' .proxy.yaml
-clash &
 
 sudo pacman -S --needed --noconfirm `cat .packages`
 sudo pacman -Rs --noconfirm xterm nano
@@ -37,6 +37,8 @@ for repo in github gitlab; do
 	mkdir -p .ssh/$repo
 	ssh-keygen -t rsa -C $mail -N '' -f .ssh/$repo/id_rsa
 done
+
+chsh -s `which zsh`
 
 systemctl enable bluetooth.service
 # for device in D1:00:FF:11:15:5B 20:73:34:03:20:94; do
