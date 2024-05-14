c4ba687b795a8b0102eba643d5b394caa71d5183
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-05-14 23:58:10 +0800

    Almost all i3 & sway configs have been transfered to under hyprland!

--- .hotkey.sh
+++ .hotkey.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo -e "$(date +%s)\t$1\t$2" >> ~/.hotkey_log
-i3-msg "$2"
+case $XDG_CURRENT_DESKTOP in
+    Hyprland)
+	hyprctl dispatch "$2"
+        ;;
+    *)
+        i3-msg "$2"
+        ;;
+esac
