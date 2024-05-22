b172586c6065c4849c67b19490c9a44a40d5ca15
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-05-22 19:41:59 +0800

    After studying with wev and kitten showmap utility, .focus.sh is more robust now. 1 sec. delay applied.

--- .focus.sh
+++ .focus.sh
@@ -1,17 +1,14 @@
-class=`hyprctl activewindow -j | jq '.class'`
-title=`hyprctl activewindow -j | jq '.title'`
+info=`hyprctl activewindow -j`
+workspace=`echo $info | jq .workspace.name | sed 's/"//g'`
+class=`echo $info | jq .class | sed 's/"//g'`
+title=`echo $info | jq .title | sed 's/"//g'`
 
-if [ "$class" != '"kitty"' ]; then
-    ydotool key 133:1 133:0
-# else
-    # ydotool key 29:1 42:1 46:1 46:0 42:0 29:0
-    # ydotool key 29 42 46
-fi
-sleep 1
+sleep .5; ydotool key 29:1 46:1 46:0 29:0; sleep .5
 
+echo -e "$(date '+%F %T')\t$1\t$workspace\t$class\t$title\t`wl-paste`" >> ~/.focus_log
 case $1 in
     note)
-        wl-paste > Documents/Obsidian/速记/$(date +%F_%T)_$title.md
+        wl-paste -n > Documents/Obsidian/速记/`date +%s`_"$workspace"_"$class"_"$title".md
         ;;
     search)
         google-chrome-stable --new-window "https://www.google.com/search?q=`wl-paste`"
