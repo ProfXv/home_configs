c64e268c247155a318d7e74af750e1a561694314
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-05-24 01:45:20 +0800

    Primary selection enabled! Copy on selection, and then enjoy the middle key operations.

--- .focus.sh
+++ .focus.sh
@@ -3,21 +3,19 @@ workspace=`echo $info | jq .workspace.name | sed 's/"//g'`
 class=`echo $info | jq .class | sed 's/"//g'`
 title=`echo $info | jq .title | sed 's/"//g'`
 
-sleep .5; ydotool key 29:1 46:1 46:0 29:0; sleep .5
-
-echo -e "$(date '+%F %T')\t$1\t$workspace\t$class\t$title\t`wl-paste`" >> ~/.focus_log
+echo -e "$(date '+%F %T')\t$1\t$workspace\t$class\t$title\t`wl-paste -p`" >> ~/.focus_log
 case $1 in
     note)
-        wl-paste -n > Documents/Obsidian/速记/`date +%s`_"$workspace"_"$class"_"$title".md
+        wl-paste -pn > Documents/Obsidian/速记/`date +%s`_"$workspace"_"$class"_"$title".md
         ;;
     search)
-        google-chrome-stable --new-window "https://www.google.com/search?q=`wl-paste`"
+        google-chrome-stable --new-window "https://www.google.com/search?q=`wl-paste -p`"
         ;;
     generate)
-        kitty --hold python .service/chat.py
+        kitty --hold python .service/generate.py -m "`wl-paste -p`"
         ;;
     ask)
-        kitty --hold python .service/chat.py -p
+        kitty --hold python .service/generate.py -p -m "`wl-paste -p`"
         ;;
     *)
         echo INVALID OPTION: $1
