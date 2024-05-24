cd84da2677cd7e4fe32c48c9e1029d615ed236e0
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-05-24 19:49:41 +0800

    Automatic search mode selection; some changes on start-up apps.

--- .focus.sh
+++ .focus.sh
@@ -3,19 +3,23 @@ workspace=`echo $info | jq .workspace.name | sed 's/"//g'`
 class=`echo $info | jq .class | sed 's/"//g'`
 title=`echo $info | jq .title | sed 's/"//g'`
 
-echo -e "$(date '+%F %T')\t$1\t$workspace\t$class\t$title\t`wl-paste -p`" >> ~/.focus_log
+
+text=$(wl-paste -p)
+echo -e "$(date '+%F %T')\t$1\t$workspace\t$class\t$title\t$text" >> ~/.focus_log
 case $1 in
     note)
-        wl-paste -pn > Documents/Obsidian/速记/`date +%s`_"$workspace"_"$class"_"$title".md
+        echo $text > Documents/Obsidian/速记/`date +%s`_"$workspace"_"$class"_"$title".md
         ;;
     search)
-        google-chrome-stable --new-window "https://www.google.com/search?q=`wl-paste -p`"
+	grep -E '^(https?://)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*/?$' <<< "$text"
+        if [ $? -eq 0 ]; then query="$text"; else query="https://www.google.com/search?q=$text"; fi
+        google-chrome-stable --new-window "$query"
         ;;
     generate)
-        kitty --hold python .service/generate.py -m "`wl-paste -p`"
+        kitty --hold python .service/generate.py -m "$text"
         ;;
     ask)
-        kitty --hold python .service/generate.py -p -m "`wl-paste -p`"
+        kitty --hold python .service/generate.py -p -m "$text"
         ;;
     *)
         echo INVALID OPTION: $1
