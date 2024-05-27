661b6c5192c560b8e31be3905b8bfbf13cd63cb6
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-05-27 15:58:28 +0800

    After archinstall resolution.

--- .init.sh
+++ .init.sh
@@ -1,5 +1,6 @@
 echo WARNING: ONLY BARE MACHINES CAN EXECUTE THIS SCRIPT FOR AUTOMATIC CONFIGURATION
 echo By typing your password, you admit you are fully aware of the risk, and we start.
+cd ~
 
 sudo sed -i 's/#zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/g' /etc/locale.gen
 sudo locale-gen
@@ -10,16 +11,14 @@ curl https://update.glados-config.com/clash/290141/765f3fd/50821/glados-terminal
 sed -i 's/external-ui: dashboard/# external-ui: dashboard/g' .config/clash/config.yaml
 clash -d .config/clash/ &
 
-cd ~
 git clone -n https://github.com/ProfXv/home_configs
 mv -i home_configs/.git/ .
 rm -r home_configs/
 git checkout -f
-git remote remove origin
-git remote add origin git@github.com:ProfXv/home_configs.git
+git remote set-url origin git@github.com:ProfXv/home_configs.git
 
-sudo pacman -S --needed --noconfirm `cat .packages`
-sudo pacman -Rs --noconfirm xterm nano
+for p in `cat .packages`; do sudo pacman -S --needed --noconfirm $p || echo $p >> failures.txt; done
+sudo pacman -R --noconfirm nano
 
 git clone https://aur.archlinux.org/yay.git
 cd yay 
@@ -40,6 +39,7 @@ done
 
 chsh -s `which zsh`
 
+iwctl station wlan0 connect Gaia-5G
 systemctl enable bluetooth.service
 systemctl enable runsunloginclient.service
 # for device in D1:00:FF:11:15:5B 20:73:34:03:20:94; do
@@ -55,4 +55,4 @@ systemctl enable runsunloginclient.service
 # 
 # for device in D1:00:FF:11:15:5B 20:73:34:03:20:94; do for act in pair connect trust; do bluetoothctl $act $device; done; done
 
-reboot
+echo Initialization Completed.
