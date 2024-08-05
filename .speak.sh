096620c1b6989712498af018015dc7adb8012b49
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-08-06 07:26:25 +0800

    Proxy & IM variables update; Magic workspace for showing logs; Hyprland socket2 basic handling and more...

--- .speak.sh
+++ .speak.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+hyprctl notify -1 1000 "rgb(ff1ea3)" "Start Recognition."
 cd .asr/bin
 words=$(./iat_online_record_sample)
 
-if [ "$1" = "simple" ]; then
-    wl-copy -p "$words"
-    ydotool click 0xC2
-elif [ "$1" = "complex" ]; then
-    echo "$words" | sed "s/这个/$(wl-paste -p)/" > /tmp/words
-    wl-copy -p < /tmp/words
-    hyprctl notify -1 2000 "rgb(ffffa3)" "Transformation Completed."
+if [ -n $words ]; then
+    echo -e `date +'%F %T'`\\t$words >> ~/.words.txt
+    if [ "$1" = "simple" ]; then
+        wl-copy -p "$words"
+        ydotool click 0xC2
+    elif [ "$1" = "complex" ]; then
+        echo "$words" | sed "s/这个/$(wl-paste -p)/" > /tmp/words
+        wl-copy -p < /tmp/words
+    fi
+    hyprctl notify -1 2000 "rgb(ffffa3)" "End Recognition."
 fi
