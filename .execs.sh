824329a06629a32ba47d5a645ab50cda2e8e604d
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-10-24 23:44:31 +0800

    fix: .exec.sh can correctly parse code with "||" etc.

--- .execs.sh
+++ .execs.sh
@@ -1,10 +1,20 @@
 # Set programs that you use
 te=kitty
 eo=rifle
-al='$pkill wofi || wofi'
+al='pkill wofi || wofi'
 
 pkill fzf || {
-    operation=`grep ^bind .config/hypr/* | fzf | awk -F, '{print $3 $4}'`
+    operation=$(grep ^bind .config/hypr/* | fzf | awk -F, '
+        {
+            printf "\""
+            for (i = 3; i <= NF; i++) {
+                gsub(/^ *| *$/, "", $i)
+                printf "%s ", $i
+            }
+            printf "\""
+        }
+    ')
     hyprctl dispatch focuscurrentorlast
     eval hyprctl dispatch $operation
+    kill $KITTY_PID
 }
