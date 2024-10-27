cec7cd8e68617c0a34ff23d108a3fbe59b8b79da
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-10-27 22:42:36 +0800

    feat: save current site if site not selected when exploring

--- .explore.sh
+++ .explore.sh
@@ -1,3 +1,13 @@
 cd .sites
-sites=`ls | fzf` && site=`cat $sites | fzf` && nohup rifle $site &
+sites=`ls | fzf`
+site=`cat $sites | fzf`
+if [ -n "$site" ]; then
+    nohup rifle $site &
+else
+    # assuming in a browser
+    hyprctl dispatch focuscurrentorlast
+    ydotool key 29:1 38:1 38:0 29:0 && sleep .5
+    text=$(xclip -o)
+    curl "$text" && echo $text >> $sites
+fi
 kill $KITTY_PID
