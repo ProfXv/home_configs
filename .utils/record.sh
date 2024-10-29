3ac85a48f2e171b8e33b5592744e7a1829df47d2
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-10-29 19:07:13 +0800

    feat: window selection when capture / record windows

--- .utils/record.sh
+++ .utils/record.sh
@@ -1,29 +1,57 @@
 #!/bin/bash
 
-cd "`~/.path.sh`"
+cd "`~/.utils/path.sh`"
+
+# two functions below are adapted from https://github.com/Gustash/Hyprshot
+function slurp_window() {
+    local name=`hyprctl -j monitors | jq -r 'map(.activeWorkspace.id) | join(",")'`
+    local clients=`hyprctl -j clients | jq -r '[.[] | select(.workspace.id | contains('$name'))]'`
+    local boxes="$(echo $clients | jq -r '.[] | "\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')"
+    slurp -r <<< "$boxes"
+}
+
+function slurp_active_window() {
+    local active_window=`hyprctl -j activewindow`
+    local box=$(echo $active_window | jq -r '"\(.at[0]),\(.at[1]) \(.size[0])x\(.size[1])"')
+    echo "$box"
+}
 
 action=$1
 area=$2
 case $action in
     "capture")
         mkdir -p Pictures
+        path=Pictures/`date +"%s.png"`
         case $area in
             "full")
-                grim Pictures/`date +"%s.png"`
+                grim $path
+                ;;
+            "window-active")
+                grim -g "`slurp_active_window`" $path
+                ;;
+            "window-select")
+                grim -g "`slurp_window`" $path
                 ;;
             "select")
-                grim -g "`slurp`" Pictures/`date +"%s.png"`
+                grim -g "`slurp`" $path
                 ;;
         esac
         ;;
     "record")
         mkdir -p Videos
+        path=Videos/`date +"%s.mp4"`
         case $area in
             "full")
-                pkill wf-recorder || wf-recorder -f Videos/`date +"%s.mp4"`
+                pkill wf-recorder || wf-recorder -f $path
+                ;;
+            "window-active")
+                pkill wf-recorder || wf-recorder -f $path -g "`slurp_active_window`"
+                ;;
+            "window-select")
+                pkill wf-recorder || wf-recorder -f $path -g "`slurp_window`"
                 ;;
             "select")
-                pkill wf-recorder || wf-recorder -g "`slurp`" -f Videos/`date +"%s.mp4"`
+                pkill wf-recorder || wf-recorder -f $path -g "`slurp`"
                 ;;
         esac
         ;;
