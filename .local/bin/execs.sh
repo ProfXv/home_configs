077e92f237eb8b77ab607a5b13bc3f0a0939e8e1
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-11-18 00:21:24 +0800

    feat: using hyprctl binds to select actions is more elegant

--- .local/bin/execs.sh
+++ .local/bin/execs.sh
@@ -5,18 +5,19 @@ te="source path.sh; kitty"
 env="source .python/bin/activate; source path.sh;"
 sw="[float; size 960 540]"
 
-#@ later we could use built-in hyprctl binds -j to optimize this script
-operation=$(grep ^bind ~/.config/hypr/* | fzf | awk -F, '
-    {
-        printf "\""
-        for (i = 3; i <= NF; i++) {
-            gsub(/^ *| *$/, "", $i)
-            printf "%s ", $i
-        }
-        printf "\""
-    }
-')
+operation=$(
+    hyprctl binds -j | jq -r '.[] | (
+      (if .locked then " " else "" end) +
+      (if .mouse then "󰍽 " else "" end) +
+      (if .release then "󰕰 " else "" end) +
+      (if .repeat then " " else "" end) +
+      (if .non_consuming then "⦽ " else "" end) +
+      (if .catch_all then "󰚾 " else "" end) +
+      "\(.modmask) \(.key) \(.keycode) 󱊨 \(.dispatcher) \(.arg)"
+    )' | fzf | sed 's/.*󱊨 //'
+)
+
 windows=`hyprctl activeworkspace -j | jq .windows`
 if [ $windows != 1 ]; then hyprctl dispatch focuscurrentorlast; fi
-eval hyprctl dispatch $operation
+eval hyprctl dispatch "\"$operation\""
 kill $KITTY_PID
