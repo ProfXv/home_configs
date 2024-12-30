c430d14770108ac187371121ff1f4121524c3c59
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2024-12-30 11:28:22 +0800

    Adapt scripts for DeepSeek model integration, enhancing tool call handling and simplifying voice command execution.

--- .local/bin/voice_commands.sh
+++ .local/bin/voice_commands.sh
@@ -9,18 +9,20 @@ RESPONSE_FILE=/tmp/operations/operations.md
 RESPONSE_STATE=false
 
 operations=`hyprctl binds -j | jq '.[] | select(.has_description) | .description' | nl`
-echo $operations
+content="Operation succeeded."
 
 mkdir -p /tmp/operations
-append_to_conversation -r system -c "Using the given tool to select operation by query from the list below:\n$operations"
+append_to_conversation -r system -c "选择一个最合适的操作:\n$operations"
 append_to_conversation -r user -c "`~/.asr/bin/iat_online_record_sample`"
 
 response=`send_request -t select_operation_number`
+echo -E $response | jq
 content=`echo -E $response | jq -r '.choices[0].message.content'`
 number=`echo -E $response | jq '.choices[0].message.tool_calls[0].function.arguments | fromjson | .number'`
 
 operation=`echo $operations | sed -n "$number"p | grep -o '".*"'`
 script=$(hyprctl binds -j | jq -r '.[] | select(.description == '$operation') | ("\(.dispatcher) \(.arg)")')
 
-notify-send "$content" "$script"
+notify-send "$script" "$content"
 eval hyprctl dispatch "\"$script\""
+rm $CONVERSATION_FILE
