2302e728faa031ca48eba6b7573ad59181e3fb85
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2025-01-09 12:39:43 +0800

    feat: support multiple operations in a single function call for both terminal chat and voice commands

--- .local/bin/voice_commands.sh
+++ .local/bin/voice_commands.sh
@@ -12,17 +12,20 @@ operations=`hyprctl binds -j | jq '.[] | select(.has_description) | .description
 content="Operation succeeded."
 
 mkdir -p /tmp/operations
-append_to_conversation -r system -c "选择一个最合适的操作:\n$operations"
+append_to_conversation -r system -c "选择最合适的操作:\n$operations"
 append_to_conversation -r user -c "`~/.asr/bin/iat_online_record_sample`"
 
 response=`send_request -t select_operation_number`
 echo -E $response | jq
 content=`echo -E $response | jq -r '.choices[0].message.content'`
-number=`echo -E $response | jq '.choices[0].message.tool_calls[0].function.arguments | fromjson | .number'`
+numbers=`echo -E $response | jq '.choices[0].message.tool_calls[].function.arguments | fromjson | .number'`
+if [ -n "$content" ]; then notify-send Reply "$content"; fi
 
-operation=`echo $operations | sed -n "$number"p | grep -o '".*"'`
-script=$(hyprctl binds -j | jq -r '.[] | select(.description == '$operation') | ("\(.dispatcher) \(.arg)")')
+for number in `echo $numbers`; do
+    operation=`echo $operations | sed -n "$number"p | grep -o '".*"'`
+    script=$(hyprctl binds -j | jq -r '.[] | select(.description == '$operation') | ("\(.dispatcher) \(.arg)")')
+    notify-send Operation "$script"
+    eval hyprctl dispatch "\"$script\""
+done
 
-notify-send "$script" "$content"
-eval hyprctl dispatch "\"$script\""
 rm $CONVERSATION_FILE
