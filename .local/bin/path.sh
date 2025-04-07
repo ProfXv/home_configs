4324bf676d4e8bc0548aa8876f11f649ab5d73d4
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2025-04-07 16:21:10 +0800

    chore: shift directory initialization from path.sh to rename.sh for workspace setup

--- .local/bin/path.sh
+++ .local/bin/path.sh
@@ -4,5 +4,5 @@ path=~/ORIGINAL
 workspace_id=$(hyprctl activeworkspace -j | jq .id)
 workspace_name=$(hyprctl activeworkspace -j | jq -r .name)
 echo $workspace_id | grep -v - && [ "$workspace_id" != "$workspace_name" ] &&
-path=$path/Desktop/Projects/ours/$workspace_name && mkdir -p "$path" && cd "$path"
+path=$path/Desktop/Projects/ours/$workspace_name && cd "$path"
 export PROJECT_HOME=$path
