da9240b7192a9b0f3c74b12f81bf7e4d465a357a
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2025-04-07 16:33:08 +0800

    feat: enhance URL handling with voice naming

--- .local/bin/explore.sh
+++ .local/bin/explore.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sites="$(for f in `ls Sites.txt ~/.sites/*`; do cat $f; done | fzf -m)"
+sites="$(for f in `ls Sites.txt ~/.sites/*`; do cat $f; done | fzf -m | cut -f 2)"
 if [ -n "$sites" ]; then for site in $sites; do nohup firefox --new-window $site > /dev/null & done fi
 sleep .1
 kill $KITTY_PID
--- .local/bin/save.sh
+++ .local/bin/save.sh
@@ -1,5 +1,8 @@
 # assuming in a browser
 sleep .5
 ydotool key -d 100 29:1 38:1 38:0 46:1 46:0 29:0 && sleep .5
+hyprctl notify -1 1000 "rgb(ff1ea3)" "Give a name if you want."
+name=`~/.asr/bin/iat_online_record_sample | sed 's/.$//'`
+hyprctl notify -1 1000 "rgb(ff1ea3)" "Name already given."
 text=$(xclip -o)
-curl "$text" && echo $text >> Sites.txt
+curl "$text" && echo -e $name\\t$text >> Sites.txt
