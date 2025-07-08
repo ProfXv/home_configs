459fb43905c5caae3106eb5e77908bc1c7689a1c
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2025-07-08 16:58:54 +0800

    feat: implement a reminder system with audio playback, triggerable by voice command

--- .local/bin/voice_commands.sh
+++ .local/bin/voice_commands.sh
@@ -16,17 +16,34 @@ mkdir -p /tmp/operations
 append_to_conversation -r system -c "候选操作:\n$operations"
 append_to_conversation -r user -c "`~/.asr/bin/iat_online_record_sample`"
 
-response=`send_request -t select_operation_number`
+response=`send_request -t select_operation_number -t set_reminder`
 echo -E $response | jq
 content=`echo -E $response | jq -r '.choices[0].message.content'`
-numbers=`echo -E $response | jq '.choices[0].message.tool_calls[].function.arguments | fromjson | .number'`
+tool_calls=`echo -E $response | jq '.choices[0].message.tool_calls'`
 if [ -n "$content" ]; then notify-send Reply "$content"; fi
 
-for number in `echo $numbers`; do
-    operation=`echo $operations | sed -n "$number"p | grep -o '".*"'`
-    script=$(hyprctl binds -j | jq -r '.[] | select(.description == '$operation') | ("\(.dispatcher) \(.arg)")')
-    notify-send Operation "$script"
-    eval hyprctl dispatch "\"$script\""
-done
+
+if [ -n "$tool_calls" ]; then
+    echo -E $tool_calls | jq -c '.[]' | while read -r tool_call; do
+        name=`echo -E $tool_call | jq -r '.function.name'`
+        arguments=`echo -E $tool_call | jq -r '.function.arguments'`
+
+        case "$name" in
+            select_operation_number)
+                number=`echo -E $arguments | jq -r '.number'`
+                operation=`echo $operations | sed -n "$number"p | grep -o '\".*\"'`
+                script=`hyprctl binds -j | jq -r '.[] | select(.description == '$operation') | ("\(.dispatcher) \(.arg)")'`
+                notify-send Operation "$script"
+                eval hyprctl dispatch "\"$script\""
+                ;;
+            set_reminder)
+                time=`echo -E $arguments | jq -r '.time'`
+                message=`echo -E $arguments | jq -r '.message'`
+                set_reminder.sh "$time" "$message"
+                notify-send "Reminder Set" "Your reminder is set for '$time'."
+                ;;
+        esac
+    done
+fi
 
 rm $CONVERSATION_FILE
