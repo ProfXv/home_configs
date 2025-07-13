b28bf1dad58758c838893050f663979df5c6fe85
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2025-07-13 11:14:50 +0800

    feat: enhance gemini integration and hyprland startup

--- .local/bin/launch_gemini.sh
+++ .local/bin/launch_gemini.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-eval "export GEMINI_API_KEY=\$GEMINI_API_KEY_$(printf "%s\n" {1..9} | fzf)"; gemini
+eval "export GEMINI_API_KEY=\$GEMINI_API_KEY_$(printf "%s\n" {1..9} | fzf)"
+if [ "$PROJECT_HOME" = "$HOME" ]; then gemini; else gemini -y; fi
