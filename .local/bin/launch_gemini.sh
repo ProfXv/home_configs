1246e2d7588fd9a38602ccf2f932934476651e82
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2025-07-21 02:50:25 +0800

    feat: add gemini api usage tracking and enhance selection view

--- .local/bin/launch_gemini.sh
+++ .local/bin/launch_gemini.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-eval "export GEMINI_API_KEY=\$GEMINI_API_KEY_$(printf "%s\n" {1..9} | fzf)"
-if [ "$PROJECT_HOME" = "$HOME" ]; then gemini; else gemini -y; fi
+number=`gemini_usage.awk ~/.gemini_usage.tsv | column -t | fzf | awk '{print $1}'`
+eval export GEMINI_API_KEY=\$GEMINI_API_KEY_$number
+model=`printf "pro\nflash" | fzf` && model="gemini-2.5-"$model
+if [ "$PROJECT_HOME" = "$HOME" ]; then gemini -m $model; else gemini -ym $model; fi
