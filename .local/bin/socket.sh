11a14be5dd16fafec8e9d414496bec559b8a468e
Paradoxist@Paradoxer <nparadoxist@gmail.com>
2025-08-02 01:28:45 +0800

    feat: introduce border color flicker effect to reflect resource usage intensity

--- .local/bin/socket.sh
+++ .local/bin/socket.sh
@@ -48,21 +48,33 @@ visualize() {
     infos=`hyprctl clients -j | jq '.[] | select(.address == "'$addr'")'`
     `echo $infos | jq '.floating or .pseudo'` || {
         pid=`echo $infos | jq '.pid'`
-        pids=`ps --ppid $pid --pid $pid -o pid --no-headers`
-        while true; do
-            read cpu mem <<< $(echo "$pids" | xargs -I{} top -b -n 1 -p {} | awk '
-            /^ *[0-9]+ / {
-                cpu += $9 + 0;
-                mem += $10 + 0;
-            }
-            END {
-                if (cpu != "" && mem != "") {
-                    if (cpu > 100) {cpu = 100}
-                    print int(cpu/100*255+.5), int(mem/100*255+.5);
+        pids=$(ps --ppid $pid --pid $pid -o pid --no-headers | tr '\n' ',')
+        last_time=$(date +%s.%N)
+        total_cpu=0
+        total_mem=0
+        while ps -p $pid > /dev/null; do
+            current_time=$(date +%s.%N)
+            time_delta=$(awk -v start="$last_time" -v end="$current_time" 'BEGIN { print end - start }')
+            last_time=$current_time
+            cpu=$(top -b -n 1 -p "$pids" | awk '/^ *[0-9]+ / {cpu += $9 + 0} END {print cpu}')
+            mem=$(top -b -n 1 -p "$pids" | awk '/^ *[0-9]+ / {mem += $10 + 0} END {print mem}')
+            read cpu_color_level mem_color_level total_cpu total_mem <<< $(awk \
+                -v cpu="$cpu" \
+                -v mem="$mem" \
+                -v time_delta="$time_delta" \
+                -v total_cpu="$total_cpu" \
+                -v total_mem="$total_mem" '
+                function abs(v) { return v < 0 ? -v : v }
+                function calc_value(x) { frac = x - int(x); return 1 - (abs(frac - 0.5) / 0.5); }
+                BEGIN {
+                    total_cpu += (cpu * time_delta) / 100;
+                    total_mem += (mem * time_delta) / 100;
+                    cpu_frac = calc_value(total_cpu);
+                    mem_frac = calc_value(total_mem);
+                    print int(cpu_frac * 255 + 0.5), int(mem_frac * 255 + 0.5), total_cpu, total_mem;
                 }
-            }')
-            [ -z "$cpu$mem" ] && break
-            color="rgb(`printf "%02x" $cpu``printf "%02x" $mem`00)"
+            ')
+            color="rgb($(printf "%02x%02x00" "$mem_color_level" "$cpu_color_level"))"
             for var in '' in; do
                 hyprctl setprop -q address:$addr "$var"activebordercolor $color
             done
