d3d8e614fd9f444d303da5478e86b2696a93082c
ProfXv <nparadoxist@gmail.com>
2026-02-28 17:14:33 +0800

    refactor: replace read prompt with fzf in rename.sh

--- modules/local/bin/rename.sh
+++ modules/local/bin/rename.sh
@@ -19,7 +19,9 @@ echo "🌟 Hey there! Let's give your workspace a fresh new name! 🌟"
         sleep 3
     }
 } || {
-    read -p "Enter the cool new name (or press Enter to select from existing ones): " new_name
-    [ -n "$new_name" ] && run_project.sh CAO --init "$new_name" || new_name=$(ls -t | fzf) &&
-    [ -n "$new_name" ] && hyprctl dispatch renameworkspace $old_id "$new_name"
+    new_name=$(ls -t | fzf --prompt="Select project or type new name: ")
+    [ -n "$new_name" ] && {
+        ls -t | grep -Fxq "$new_name" || run_project.sh CAO --init "$new_name"
+        hyprctl dispatch renameworkspace $old_id "$new_name"
+    }
 }
